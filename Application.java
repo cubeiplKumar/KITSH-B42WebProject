47d8e0b3b127d9a5b7d93dc4bb7da39057bea8e8
cubeiplkumar <emailanilkumarm@gmail.com>
2025-03-24 13:03:30 +0530

    feature202 changes

--- Application.java
+++ Application.java
@@ -19,4 +19,23 @@ public class Example {
   private static void report(String day) {
     System.out.printf("Welcome to the zombie %s\n", day);
   }
+  #feature202 changes by vivek on october 2024 on reports module
+  public static int square(int num) {
+
+    // return statement
+    return num * num;
+  }
+
+  public static void main(String[] args) {
+    int result;
+
+    // call the method
+    // store returned value to result
+    result = square(10);
+
+    System.out.println("Squared value of 10 is: " + result);
+  }
+}
+#feature202 code ends here
+
 }
