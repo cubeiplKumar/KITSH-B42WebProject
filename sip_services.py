407260396d74d8f004cb33a3b533a0ada6d614dc
cubeiplKumar <34553586+cubeiplKumar@users.noreply.github.com>
2025-04-02 12:35:36 +0530

    Update sip_services.py

--- sip_services.py
+++ sip_services.py
@@ -1,2 +1,19 @@
 Need to write the code for
 SIP investments and its relevant services
+#JENKINS-WEBHOOK JOB DEMO
+# Function returning a tuple
+
+def get_student_info():
+
+    name = “John”
+
+    grade = “A”
+
+    age = 20
+
+    return name, grade, age  # Returns a tuple
+
+student_info = get_student_info()
+
+print(student_info)          # Output: (‘John’, ‘A’, 20)
+#END OF WEBHOOK JOB
