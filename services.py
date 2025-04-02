1da29d6b32286d61b7145b2da4190eccd5226247
cubeiplKumar <34553586+cubeiplKumar@users.noreply.github.com>
2025-04-02 12:23:49 +0530

    Update services.py

--- services.py
+++ services.py
@@ -4,3 +4,9 @@
 import random
 
 print(random.randint(0,9))
+
+#JENKINS AUTOBUILD JOBDEMO
+
+# calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
